892eb1c8a08086599c4bcda4b60f69085e8ee07e
HuyVu2379 <21021491.huy@student.iuh.edu.vn>
2025-01-15 13:45:20 +0700

    feat: Complete header (login functionality pending)

--- src/components/Shared/HeaderLanding/index.tsx
+++ src/components/Shared/HeaderLanding/index.tsx
@@ -1,7 +1,52 @@
-import { Box } from "@mui/material";
+import { Box, Link, AppBar, Toolbar } from '@mui/material';
+import Logo from '../../../assets/Logo.svg';
+const HeaderLanding = () => {
+  return (
+    <AppBar position="static" color="inherit" elevation={0} sx={{ borderBottom: '1px solid #ddd' }}>
+      <Toolbar sx={{ justifyContent: 'space-between', padding: '0 20px' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <img src={Logo} alt="Logo" style={{ height: '30px' }} />
+        </Box>
+        <Box sx={{ display: 'flex', gap: 3 }}>
+          <Link href="#" underline="none" sx={linkStyle}>
+            ZALO PC
+          </Link>
+          <Link href="#" underline="none" sx={linkStyle}>
+            OFFICIAL ACCOUNT
+          </Link>
+          <Link href="#" underline="none" sx={linkStyle}>
+            NHÀ PHÁT TRIỂN
+          </Link>
+          <Link href="#" underline="none" sx={linkStyle}>
+            BẢO MẬT
+          </Link>
+          <Link href="#" underline="none" sx={linkStyle}>
+            TRỢ GIÚP
+          </Link>
+          <Link href="#" underline="none" sx={linkStyle}>
+            LIÊN HỆ
+          </Link>
+          <Link href="#" underline="none" sx={linkStyle}>
+            BÁO CÁO VI PHẠM
+          </Link>
+          <Link href="#" underline="none" sx={{ ...linkStyle, color: '#0078FF', fontWeight: 'bold' }}>
+            ĐĂNG NHẬP
+          </Link>
+        </Box>
+        <Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-function HeaderLanding() {
-  return <Box>HeaderLanding</Box>;
-}
+const linkStyle = {
+  fontSize: '13px',
+  color: '#000',
+  fontWeight: '600',
+  '&:hover': {
+    color: '#0078FF',
+  },
+};
 
 export default HeaderLanding;
