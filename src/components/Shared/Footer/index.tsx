8544a4bbd8e02cdc4b4ac25dc9d4ee965cfc261b
HuyVu2379 <21021491.huy@student.iuh.edu.vn>
2025-01-15 13:48:06 +0700

    feat: Add footer

--- src/components/Shared/Footer/index.tsx
+++ src/components/Shared/Footer/index.tsx
@@ -1,7 +1,41 @@
-import { Box } from "@mui/material";
+import { Box, Link, Typography } from '@mui/material';
 
-function Footer() {
-  return <Box>Footer</Box>;
-}
+const Footer = () => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        textAlign: 'center',
+        padding: '10px 0',
+        borderTop: '1px solid #ddd',
+      }}
+    >
+      <Typography variant="body2">
+        © 2012 - 2025 Một sản phẩm của Zalo Group -{' '}
+        <Link href="#" underline='none'
+          sx={{
+            color: "primary.main",
+            '&:hover': {
+              color: "#084ea6"
+            }
+          }}
+        >
+          Điều khoản sử dụng dịch vụ
+        </Link>{' '}
+        -{' '}
+        <Link href="#" underline='none'
+          sx={{
+            color: "primary.main",
+            '&:hover': {
+              color: "#084ea6"
+            }
+          }}
+        >
+          Thông báo xử lý dữ liệu
+        </Link>
+      </Typography>
+    </Box >
+  );
+};
 
 export default Footer;
