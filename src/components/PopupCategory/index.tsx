cf9baaf91d1864b456667c2207750708aae92915
LeQuocPhong2k2 <phonglehn2002@gmai.com>
2025-02-03 22:19:23 +0700

    implement popup category <phonglq>

---
+++ src/components/PopupCategory/index.tsx
@@ -0,0 +1,9 @@
+function PopupCategory() {
+  return (
+    <div>
+      <h1>Popup Category</h1>
+    </div>
+  );
+}
+
+export default PopupCategory;
--- src/pages/pc/Content/index.tsx
+++ src/pages/pc/Content/index.tsx
@@ -2,21 +2,19 @@ import { APP_ROUTES } from "@/constants";
 import { Box, Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
+import PopupCategory from "@/components/PopupCategory";
+
 function ContentPCLandingTemplate() {
   const navigate = useNavigate();
   return (
     <Box sx={{ my: 10 }}>
       {" "}
+      <PopupCategory />
       <Typography variant="body1" color="initial">
-        Tải Zalo PC cho máy tính Ứng dụng Zalo PC đã có mặt trên Windows, Mac
-        OS, Web
+        Tải Zalo PC cho máy tính Ứng dụng Zalo PC đã có mặt trên Windows, Mac OS, Web
       </Typography>
       <Button color="primary">Tải ngay</Button>
-      <Button
-        variant="outlined"
-        color="primary"
-        onClick={() => navigate(APP_ROUTES.USER.LOGIN_QR)}
-      >
+      <Button variant="outlined" color="primary" onClick={() => navigate(APP_ROUTES.USER.LOGIN_QR)}>
         Dùng bản web ( test thử đăng nhập thì bấm vào đây)
       </Button>
     </Box>
