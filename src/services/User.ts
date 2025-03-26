2e857f2e7725c707f16167200b9218f8ef27a475
Quindart <quang.le@aegona.com>
2025-03-26 10:06:03 +0700

    "fix: build"

--- src/services/User.ts
+++ src/services/User.ts
@@ -2,7 +2,7 @@ import axiosConfig from "./axiosConfig";
 
 const api = "/api/v1/users/"
 
-export const getMe = async (userId) => {
+export const getMe = async (userId: any) => {
     try {
         const url = `${api}/${userId}`;
         return await axiosConfig.get(url);
